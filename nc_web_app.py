54a438032e68765a317f87de7ae34317e6348aa8
neilcu <neil.schumi@gmail.com>
2017-09-23 20:20:54 +0100

    New Webapp V1.1

--- nc_web_app.py
+++ nc_web_app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Neil {name}!</h3>" \
+    html = "<h3>NJ {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "has cracked the app"), hostname=socket.gethostname(), visits=visits)
